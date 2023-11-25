7b0902a3da5f6cd710c3af68fd3f901f9e352a73
Yine-Eng <145277882+Yine-Eng@users.noreply.github.com>
2023-11-25 21:57:48 +0000

    Initial commit

---
+++ LandTract.java
@@ -0,0 +1,60 @@
+/**
+   The LandTract class stores data about a tract of land
+   for the LandTract Class programming challenge.
+*/
+
+public class LandTract
+{
+   private double length;     // Length of the tract
+   private double width;      // Width of the tract
+   
+   /**
+      Constructor
+      @param len The length of the land tract.
+      @param w The width of the land tract.
+   */
+
+   public LandTract(double len, double w)
+   {
+      length = len;
+      width = w;
+   }
+
+   /**
+      getArea method
+      @return The area of the land tract in square feet.
+   */
+   
+   public double getArea()
+   {
+      return length * width;
+   }
+   
+   /**
+      The equals method compares two LandTract objects.
+      @param tract2 A reference to a LandTract object.
+      @return true if the two tracts are the same size,
+              false otherwise.
+   */
+      
+   public boolean equals(LandTract tract2)
+   {
+      if (getArea() == tract2.getArea())
+        return true;
+      else
+        return false;
+   }
+   
+   /**
+      toString method
+      @return A string indicating the land tract's length,
+              width, and area.
+   */
+   
+   public String toString()
+   {
+      String str = "Length: " + length + 
+                  "\nWidth: " + width + "\nArea: " + getArea();
+     return str;
+   }
+}
